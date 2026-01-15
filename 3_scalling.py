be5739d4740964780050b3c2c92482fcd0d6119c
Huyen Trang <trannguyenhuyentrang07@gmail.com>
2026-01-15 09:57:05 +0700

    #3-Scaling techniques

---
+++ 3_scalling.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+data={
+    'feature1':np.random.random(100)*100,
+    'feature2':np.random.random(100)*50,    
+    'target':np.random.choice([0,1],size=100)
+}
+df=pd.DataFrame(data)
+x=df.drop('target', axis=1)
+y=df['target']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=42)
+scaler=StandardScaler()
+x_train_scaled=scaler.fit_transform(x_train)
+x_test_scaled=scaler.transform(x_test)
+print(x_train_scaled)
---
+++ minmaxscaler.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+data={
+    'feature1':np.random.random(100)*100,
+    'feature2':np.random.random(100)*50,    
+    'target':np.random.choice([0,1],size=100)
+}
+df=pd.DataFrame(data)
+x=df.drop('target', axis=1)
+y=df['target']
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2, random_state=42)
+scaler=MinMaxScaler()
+x_train_scaled=scaler.fit_transform(x_train)
+x_test_scaler=scaler.transform(x_test)
+print(x_train_scaled)
+print(x_test_scaler.min())
+print(x_test_scaler.max())
